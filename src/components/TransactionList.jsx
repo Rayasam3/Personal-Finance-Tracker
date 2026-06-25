4bd473a35b0ae63e08192f6ef4b8247ba5e716ed
Rayasam3 <rayasamamarnathgmail.com>
2026-06-25 16:50:32 +0530

    Displayed transactions using map

--- src/components/TransactionList.jsx
+++ src/components/TransactionList.jsx
@@ -3,9 +3,22 @@ function TransactionList({transactions}) {
     <div>
         <h2>Transactions</h2>
         <p>Total Transactions: {transactions.length}</p>
-        <p>No Transactions added Yet.</p>      
+        {
+          transactions.length === 0 ?(
+            <p>No Transactions Added Yet.</p>
+          ) : (transactions.map((transaction, index) => (
+            <div key ={index}>
+              <h3>{transaction.title}</h3>
+              <p>Amount: ₹{transaction.amount}</p>
+              <p>Category: {transaction.category}</p>
+              <p>Type: {transaction.type}</p>
+              <hr />
+            </div>
+            ))
+          )
+        }
     </div>
-  )
+  );
 }
 
 export default TransactionList
