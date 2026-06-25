533891954fbb2ea7805b8418fbf3ff365d31ed54
Rayasam3 <rayasamamarnathgmail.com>
2026-06-25 16:31:40 +0530

    Added transaction form state and add transaction functionality

--- src/components/TransactionForm.jsx
+++ src/components/TransactionForm.jsx
@@ -1,42 +1,65 @@
-function TransactionForm() {
+import { useState } from "react";
+
+
+function TransactionForm({ addTransaction }) {
+  const [title,setTitle] = useState("");
+  const [amount,setAmount] = useState("");
+  const [category,setCategory] = useState("Food");
+  const [type, setType] = useState("Income");
+
+  const handleSubmit =(e)=>{
+    e.preventDefault();
+    const transaction = {
+      title,
+      amount,
+      category,
+      type,
+    };
+    addTransaction(transaction);
+
+    setTitle("");
+    setAmount("");
+    setCategory("Food");
+    setType("Income");
+  }
   return (
     <div>
       <h2>Add Transaction</h2>  
-      <form>
+      <form onSubmit={handleSubmit}>
         <div>
             <label>Title</label>
             <br />
-            <input type="text" placeholder="Enter transaction Title"/>
+            <input type="text" placeholder="Enter transaction Title" value={title} onChange={(e)=>setTitle(e.target.value)}/>
         </div>
         <br />
         <div>
             <label >Amount</label>
             <br />
-            <input type="text"placeholder="Enter Amount" />
+            <input type="number"placeholder="Enter Amount" value={amount} onChange={(e)=> setAmount(e.target.value)} />
         </div>
         <br />
         <div>
             <label >Category</label>
             <br />
-            <select name="" id="">
-                <option value="">Food</option>
-                <option value="">Travels</option>
-                <option value="">Shopping</option>
-                <option value="">Salary</option>
-                <option value="">Others</option>
+            <select name="" id="" value={category} onChange={(e)=>setCategory(e.target.value)}>
+                <option value="Food">Food</option>
+                <option value="Travels">Travels</option>
+                <option value="Shopping">Shopping</option>
+                <option value="Salary">Salary</option>
+                <option value="Others">Others</option>
             </select>
         </div>
         <br />
         <div>
             <label htmlFor="">Type</label>
             <br />
-            <select name="" id="">
-                <option value="">Income</option>
-                <option value="">Expense</option>
+            <select name="" id="" value={type} onChange={(e)=>setType(e.target.value)}>
+                <option value="Income">Income</option>
+                <option value="Expense">Expense</option>
             </select>
         </div>
         <br />
-        <button>Add Transaction</button>
+        <button type="submit">Add Transaction</button>
       </form>
     </div>
   );
--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -3,13 +3,19 @@ import SummaryCards from "../components/SummaryCards";
 import TransactionForm from "../components/TransactionForm";
 import TransactionList from "../components/TransactionList";
 import { useState } from "react";
+
 function Dashboard() {
     const [transactions, setTransactions] = useState([]);
+
+    const addTransaction = (newTransaction) => {
+    setTransactions([...transactions,newTransaction]);
+    };
+
   return (
     <div>
         <Navbar />
         <SummaryCards />
-        <TransactionForm />
+        <TransactionForm addTransaction={addTransaction}/>
         <TransactionList transactions={transactions} />
     </div>
   );
